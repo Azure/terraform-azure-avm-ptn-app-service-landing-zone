23fe83b1ee4fd0949dd7328d3b199e42cad8407a
Jared Holgate <jaredholgate@microsoft.com>
2026-03-19 15:51:24 +0000

    trigger PR checks

--- examples/managed_instance/scripts/install.ps1
+++ examples/managed_instance/scripts/install.ps1
@@ -1,7 +1,7 @@
 <#
 This is an example PowerShell script to show how to customize an App Service Managed Instance to perform additional configuration after it has been provisioned.
 In this example, the script creates a registry key with a string value, creates a JSON configuration file on the C: drive, and installs fonts from the current directory and subdirectories to the Windows Fonts folder.
-You can RDP via Azure Bastion onto one of the instances in the App Service Managed Instance to see the results after deployment.
+You can RDP via Azure Bastion onto any of the instances in the App Service Managed Instance to see the results after deployment.
 #>
 
 # Create a registry key with a string value
